37457e4f307fa0416f43a09d32284ab778cc1552
DivyanshBharadwaj <divyanshbharadwaj008@gmail.com>
2025-09-21 09:41:34 +0530

    Update __init__.py

--- data/__init__.py
+++ data/__init__.py
@@ -3,16 +3,16 @@ Data processing module for INDRA LLM
 (c) Divyansh Bharadwaj
 """
 
-from .dataset import INDRADataset, VedicDataset, MultiLanguageDataset, InstructionDataset
+from .dataset import StreamingINDRADataset, StreamingVedicDataset, StreamingInstructionDataset
 from .dataloader import INDRADataLoader, create_dataloader
 from .vedic_corpus import VedicCorpusProcessor
 
 __all__ = [
-    'INDRADataset',
-    'VedicDataset', 
-    'MultiLanguageDataset',
-    'InstructionDataset',
+    'StreamingINDRADataset',
+    'StreamingVedicDataset', 
+    'StreamingInstructionDataset',
     'INDRADataLoader',
     'create_dataloader',
     'VedicCorpusProcessor'
 ]
+
